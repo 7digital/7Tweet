2b38b4b4d621906719861a61be92480fbd303f2f
Ben Biddington <ben.biddington@gmail.com>
2010-01-07 17:17:00 +0000

    Another json parsing howto

--- test/com/sevendigital/scala/seventweet/integration/tests/how/to/ParseJson.scala
+++ test/com/sevendigital/scala/seventweet/integration/tests/how/to/ParseJson.scala
@@ -16,8 +16,33 @@ class ParseJson {
 		val expectedQuery 	= "%23musicmonday+OR+%23mm"
 		val actual 			= jsonMap get("query") get
 
+		println(jsonMap)
+
 		assertThat(actual, is(equalTo(expectedQuery)))
     }
+
+	@Test
+	def into_search_results {
+		val jsonMap = parse(MUSIC_MONDAY_SEARCH_RESULT)
+		val searchResults = jsonMap.get("results").get.asInstanceOf[List[List[(String, String)]]];
+
+		val firstResult = searchResults.first
+		
+		val (_, profileImage) 	= firstResult.first
+		val (_, createdDate) 	= firstResult(1)
+		
+		val fromUser 		= firstResult(2)
+		val toUser 			= firstResult(3)
+		val text 			= firstResult(4)
+		val id 				= firstResult(5)
+		val fromUserId 		= firstResult(6)
+		val geo 			= firstResult(7)
+		val isoLanguageCode	= firstResult(8)
+		val source 			= firstResult(9)
+
+		assertThat(profileImage, is(equalTo("http://a1.twimg.com/profile_images/591055468/PIC_2191_normal.JPG")))
+		assertThat(createdDate, is(equalTo("Thu, 07 Jan 2010 14:01:55 +0000")))
+	}
 
 	private def parse(file : String) : Map[String, String] =
 		JSON.parseFull(load(file)).get.asInstanceOf[Map[String, String]]
