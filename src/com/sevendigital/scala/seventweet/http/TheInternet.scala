0480b22f6d0c0ab9f5b55f9f29bac0633428db51
Ben Biddington <ben.biddington@gmail.com>
2010-01-12 22:12:40 +0000

    Removed imports

--- src/com/sevendigital/scala/seventweet/http/TheInternet.scala
+++ src/com/sevendigital/scala/seventweet/http/TheInternet.scala
@@ -1,9 +1,6 @@
 package com.sevendigital.scala.seventweet.http
 
 import java.net.URI
-import org.apache.commons.httpclient.NameValuePair
-import collection.mutable.ListBuffer
-
 abstract class TheInternet {
 	def get(resource : URI) : Response = get(resource, new QueryParameters())
 	def get(resource : URI, parameters : QueryParameters) : Response
