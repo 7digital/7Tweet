267336a02b1a227ec43116e994d297a6aa358d3d
unknown <rachel@.7digital.local>
2010-01-13 13:32:25 +0000

    Decribed that 100 is the limit for public search results

--- test/com/sevendigital/scala/seventweet/integration/tests/PublicSearchTests.scala
+++ test/com/sevendigital/scala/seventweet/integration/tests/PublicSearchTests.scala
@@ -1,5 +1,6 @@
 package com.sevendigital.scala.seventweet.integration.tests
 
+import java.net.URI
 import org.junit.Test
 import org.junit.Assert._
 import org.hamcrest.core.Is._
@@ -7,9 +8,8 @@ import org.hamcrest.core.IsNot._
 import org.hamcrest.core.IsEqual._
 import org.mockito.Mockito._
 import org.mockito.Matchers._
-import java.net.URI
 import com.sevendigital.scala.seventweet.twitter.PublicSearch
-import com.sevendigital.scala.seventweet.http.{SimpleInternet, Response, TheInternet}
+import com.sevendigital.scala.seventweet.http.SimpleInternet
 
 class PublicSearchTests {
 	@Test
@@ -28,6 +28,17 @@ class PublicSearchTests {
 		val results = publicSearch.search("#musicmonday", resultCount)
 
 		assertThat(results.size, is(equalTo(resultCount)))
+	}
+
+	@Test
+	def can_search_for_maximum_of_100_results {
+		val publicSearch = new PublicSearch(new SimpleInternet, SEARCH_TWITTER)
+		val results = publicSearch.search("#musicmonday", 500)
+
+		assertThat(
+			"Expected the limit for results to be 100",
+			results.size, is(equalTo(100))
+		)
 	}
 
 	val SEARCH_TWITTER = new URI("http://search.twitter.com/search.json")
